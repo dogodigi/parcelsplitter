2e57c9233a81c8ed16fad91df8ef60777b87d6f2
milovanderlinden <milo@dogodigi.net>
2015-05-13 20:44:32 +0200

    moved app js to seperate file

---
+++ public/js/parcelsplitter/parcelsplitter.js
@@ -0,0 +1,27 @@
+/* global L */
+
+/**
+ *  Copyright (c) 2014 Milo van der Linden (milo@dogodigi.net)
+ * 
+ *  This file is part of parcelsplitter
+ *  
+ *  parcelsplitter is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  parcelsplitter is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with parcelsplitter. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+var map = L.map('map').setView([-25.299398189009363, -57.619957029819496], 13);
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/miblon.map-n72dremu/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
